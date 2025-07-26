a62e9802e4fb42a6aecdda5639aafeb641d14c67
utpal-medblocks <utpal@medblocks.com>
2025-07-26 19:30:14 +0530

    added support for nested object

--- backend/src/main/java/com/couchbase/fhir/resources/util/StringSearchHelper.java
+++ backend/src/main/java/com/couchbase/fhir/resources/util/StringSearchHelper.java
@@ -1,9 +1,6 @@
 package com.couchbase.fhir.resources.util;
 
-import ca.uhn.fhir.context.BaseRuntimeChildDefinition;
-import ca.uhn.fhir.context.BaseRuntimeElementCompositeDefinition;
-import ca.uhn.fhir.context.BaseRuntimeElementDefinition;
-import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.context.*;
 import org.hl7.fhir.r4.model.StringType;
 
 import java.util.ArrayList;
@@ -11,22 +8,65 @@ import java.util.List;
 
 public class StringSearchHelper {
 
-    public static String buildStringWhereCluse(FhirContext fhirContext,String resourceType,String paramName,String value){
+    public static String buildStringWhereCluse(FhirContext fhirContext, String resourceType, String paramName, String value , RuntimeSearchParam searchParam){
+        String rawPath = searchParam.getPath();
+        String fhirPath = rawPath.replaceFirst("^" + resourceType + "\\.", "");
+        String[] pathParts = fhirPath.split("\\.");
         StringBuilder sb = new StringBuilder();
+        if(pathParts.length == 1){
+            List<String> conditions = getSubFields(fhirContext, resourceType, paramName , value);
+            if(conditions.isEmpty()) return null;
+            sb.append("ANY ").append("elem")
+                    .append(" IN ").append(paramName)
+                    .append(" SATISFIES ");
+            sb.append(String.join(" OR ", conditions));
+            sb.append(" END");
+        }else{
+            String condition = getSubFieldsComposite(fhirContext , resourceType , paramName , value , fhirPath);
+            sb.append(condition);
 
-        List<String> conditions = getSubFields(fhirContext, resourceType, paramName , value);
-        if(conditions.isEmpty()) return null;
-        sb.append("ANY ").append("elem")
-                .append(" IN ").append(paramName)
-                .append(" SATISFIES ");
-        sb.append(String.join(" OR ", conditions));
-        sb.append(" END");
+        }
+
+
+
+
         return sb.toString();
+    }
+
+
+    public static String getSubFieldsComposite(FhirContext ctx, String resource, String fieldName , String searchValue , String fhirPath) {
+        List<String> fieldConditions = new ArrayList<>();
+        String[] pathParts = fhirPath.split("\\.");
+
+        String parentField = pathParts[0];
+        String childField = pathParts[1];
+
+        String arrayAlias = "elem";
+        String escapedValue = searchValue.toLowerCase().replace("\"", "\\\"");
+        BaseRuntimeElementCompositeDefinition<?> resourceDef = (BaseRuntimeElementCompositeDefinition<?>) ctx.getResourceDefinition(resource);
+        BaseRuntimeChildDefinition parentDef = resourceDef.getChildByName(parentField);
+        BaseRuntimeElementDefinition<?> elementDef = parentDef.getChildByName(parentField);
+        BaseRuntimeElementCompositeDefinition<?> compositeDef =
+                (BaseRuntimeElementCompositeDefinition<?>) elementDef;
+        BaseRuntimeChildDefinition childDef = compositeDef.getChildByName(childField);
+
+        boolean isChildArray = childDef.getMax() == -1;
+        String alias = "elem_" + parentField;
+
+        if (isChildArray) {
+            return String.format("ANY %s IN %s SATISFIES ANY x IN %s.%s SATISFIES LOWER(x) LIKE \"%%%s%%\" END END",
+                    alias, parentField, alias, childField, escapedValue);
+        } else {
+            return String.format("ANY %s IN %s SATISFIES LOWER(%s.%s) LIKE \"%%%s%%\" END",
+                    alias, parentField, alias, childField, escapedValue);
+        }
+
     }
 
 
     public static List<String> getSubFields(FhirContext ctx, String resource, String fieldName , String searchValue) {
         List<String> fieldConditions = new ArrayList<>();
+
         String arrayAlias = "elem";
         String escapedValue = searchValue.toLowerCase().replace("\"", "\\\"");
         BaseRuntimeElementCompositeDefinition<?> resourceDef = (BaseRuntimeElementCompositeDefinition<?>) ctx.getResourceDefinition(resource);
