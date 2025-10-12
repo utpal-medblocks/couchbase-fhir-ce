82d6ad9cfe57c438b80f1e2edf87466a26d43062
utpal-medblocks <utpal@medblocks.com>
2025-10-12 11:18:30 +0530

    capability performance improvement

--- backend/src/main/java/com/couchbase/fhir/resources/provider/USCoreCapabilityProvider.java
+++ backend/src/main/java/com/couchbase/fhir/resources/provider/USCoreCapabilityProvider.java
@@ -7,11 +7,15 @@ import ca.uhn.fhir.rest.api.server.RequestDetails;
 import ca.uhn.fhir.rest.server.RestfulServer;
 import ca.uhn.fhir.rest.server.provider.ServerCapabilityStatementProvider;
 import com.couchbase.fhir.resources.constants.USCoreProfiles;
+import com.google.common.base.Stopwatch;
 import jakarta.servlet.http.HttpServletRequest;
 import org.hl7.fhir.r4.model.CapabilityStatement;
 import org.hl7.fhir.r4.model.Enumerations;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import static com.couchbase.fhir.resources.constants.USCoreProfiles.US_CORE_BASE_URL;
 
@@ -31,47 +35,60 @@ import static com.couchbase.fhir.resources.constants.USCoreProfiles.US_CORE_BASE
 public class USCoreCapabilityProvider extends ServerCapabilityStatementProvider {
 
 
+    private final FhirContext fhirContext;
+
     public USCoreCapabilityProvider(RestfulServer restfulServer) {
         super(restfulServer);
+        this.fhirContext = restfulServer.getFhirContext();
     }
 
     @Override
     public CapabilityStatement getServerConformance(HttpServletRequest request, RequestDetails requestDetails) {
-        CapabilityStatement statement = (CapabilityStatement) super.getServerConformance(request, requestDetails);
 
+        CapabilityStatement statement =  new CapabilityStatement();
+        statement.setSoftware(new CapabilityStatement.CapabilityStatementSoftwareComponent()
+                .setName("couchbase-fhir-ce")
+                .setVersion("1.0.0"));
+        statement.setFhirVersion(Enumerations.FHIRVersion._4_0_1);
+        statement.setKind(CapabilityStatement.CapabilityStatementKind.INSTANCE);
+        statement.setStatus(Enumerations.PublicationStatus.ACTIVE);
         statement.addInstantiates(USCoreProfiles.US_CORE_SERVER);
+        CapabilityStatement.CapabilityStatementRestComponent rest = new CapabilityStatement.CapabilityStatementRestComponent();
+        rest.setMode(CapabilityStatement.RestfulCapabilityMode.SERVER);
+        Set<String> resourceTypes = fhirContext.getResourceTypes();
 
-        FhirContext fhirContext = FhirContext.forR4();
-
-        List<CapabilityStatement.CapabilityStatementRestResourceComponent> resources = statement.getRestFirstRep().getResource();
-
-        for (CapabilityStatement.CapabilityStatementRestResourceComponent resource : resources) {
-            String resourceType = resource.getType();
-            String supportedProfileUrl = US_CORE_BASE_URL + resourceType.toLowerCase();
-
-            // Add the supportedProfile if not already present
-            if (resource.getSupportedProfile().stream().noneMatch(profile -> profile.getValue().equals(supportedProfileUrl))) {
-                resource.addSupportedProfile(supportedProfileUrl);
-            }
-
+        for (String resourceType : resourceTypes) {
             RuntimeResourceDefinition resourceDef = fhirContext.getResourceDefinition(resourceType);
-            List<RuntimeSearchParam> params = resourceDef.getSearchParams();
-            for (RuntimeSearchParam param : params) {
+
+            CapabilityStatement.CapabilityStatementRestResourceComponent resource =
+                    new CapabilityStatement.CapabilityStatementRestResourceComponent();
+            resource.setType(resourceType);
+
+            // Add supported US Core profile URL
+            String supportedProfileUrl = US_CORE_BASE_URL + resourceType.toLowerCase();
+            resource.addSupportedProfile(supportedProfileUrl);
+
+            resource.addInteraction()
+                    .setCode(CapabilityStatement.TypeRestfulInteraction.READ);
+            resource.addInteraction()
+                    .setCode(CapabilityStatement.TypeRestfulInteraction.SEARCHTYPE);
+
+            // Add search parameters
+            for (RuntimeSearchParam param : resourceDef.getSearchParams()) {
                 CapabilityStatement.CapabilityStatementRestResourceSearchParamComponent searchParam =
                         new CapabilityStatement.CapabilityStatementRestResourceSearchParamComponent();
-
                 searchParam.setName(param.getName());
                 searchParam.setType(Enumerations.SearchParamType.fromCode(param.getParamType().getCode()));
                 searchParam.setDocumentation("FHIRPath: " + param.getPath());
                 if (param.getUri() != null) {
                     searchParam.setDefinition(param.getUri());
                 }
-
-                resource.addSearchParam(searchParam);;
+                resource.addSearchParam(searchParam);
             }
 
+            rest.addResource(resource);
         }
-
+        statement.addRest(rest);
         return statement;
     }
 
